c14b54ef0a6f715c97ad10b8180b23712b580786
kindkiller100 <kindkiller100@gmail.com>
2022-11-14 19:13:29 +0300

    commit one

---
+++ src/main/java/com/school/school/SchoolApplication.java
@@ -0,0 +1,17 @@
+package com.school.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import java.util.HashMap;
+
+@SpringBootApplication
+public class SchoolApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SchoolApplication.class, args);
+	}
+
+}
