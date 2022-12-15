c729024b1d31fa277568b3cc60d9c8e97f334809
galkha <galkha25@gmail.com>
2022-12-15 21:05:20 +0300

    Adds Subject class and controller

---
+++ src/main/java/subjects/SubjectsController.java
@@ -0,0 +1,10 @@
+package subjects;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/subjects")
+public class SubjectsController {
+
+}
