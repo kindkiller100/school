7d6005cb80d9534bd1757a393b1f66cbc9aa39a8
kindkiller100 <kindkiller100@gmail.com>
2022-12-19 19:32:41 +0300

    added constructors

--- src/main/java/com/school/school/teachers/Teacher.java
+++ src/main/java/com/school/school/teachers/Teacher.java
@@ -14,17 +14,56 @@ public class Teacher {
     private String info;
     private boolean deleted;
 
+    public Teacher() {
+    }
+
+    public Teacher(long id,
+                   String name,
+                   String secondName,
+                   String lastName,
+                   LocalDate dateOfBirth,
+                   String gender,
+                   String telephoneNumber,
+                   String info,
+                   boolean deleted) {
+        this.id = id;
+        this.name = name;
+        this.secondName = secondName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.telephoneNumber = telephoneNumber;
+        this.info = info;
+        this.deleted = deleted;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Teacher teacher = (Teacher) o;
-        return id == teacher.id && deleted == teacher.deleted && name.equals(teacher.name) && Objects.equals(secondName, teacher.secondName) && lastName.equals(teacher.lastName) && Objects.equals(dateOfBirth, teacher.dateOfBirth) && Objects.equals(gender, teacher.gender) && telephoneNumber.equals(teacher.telephoneNumber) && Objects.equals(info, teacher.info);
+        return id == teacher.id
+                && deleted == teacher.deleted
+                && name.equals(teacher.name)
+                && Objects.equals(secondName, teacher.secondName)
+                && lastName.equals(teacher.lastName)
+                && Objects.equals(dateOfBirth, teacher.dateOfBirth)
+                && Objects.equals(gender, teacher.gender)
+                && telephoneNumber.equals(teacher.telephoneNumber)
+                && Objects.equals(info, teacher.info);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, secondName, lastName, dateOfBirth, gender, telephoneNumber, info, deleted);
+        return Objects.hash(id,
+                name,
+                secondName,
+                lastName,
+                dateOfBirth,
+                gender,
+                telephoneNumber,
+                info,
+                deleted);
     }
 
     @Override
