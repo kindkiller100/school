5b8281ce3cdaeb6032cebaf9f79d348cb68035a2
yauhen14 <buapyp@gmail.com>
2022-12-31 09:09:28 +0300

    adds Builder in class Payment

--- src/main/java/com/school/school/payments/Payment.java
+++ src/main/java/com/school/school/payments/Payment.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
-@Table(name = "payments", schema="school_db")
+@Table(name = "payments", schema = "school_db")
 public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,13 +16,16 @@ public class Payment {
     @Column(name = "student_id")
     private long studentId;
     @Column(name = "auto_identified")
-    private boolean autoIdentified;
+    private Boolean autoIdentified;
     private String info;
 
-    public Payment() {
-    }
-
-    public Payment(LocalDateTime dateTime, double sum, long studentId, boolean autoIdentified, String info) {
+    private Payment(long id,
+                    LocalDateTime dateTime,
+                    double sum,
+                    long studentId,
+                    Boolean autoIdentified,
+                    String info) {
+        this.id = id;
         this.dateTime = dateTime;
         this.sum = sum;
         this.studentId = studentId;
@@ -46,7 +49,7 @@ public class Payment {
         return studentId;
     }
 
-    public boolean isAutoIdentified() {
+    public Boolean isAutoIdentified() {
         return autoIdentified;
     }
 
@@ -59,7 +62,7 @@ public class Payment {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Payment payment = (Payment) o;
-        return id == payment.id && Double.compare(payment.sum, sum) == 0 && studentId == payment.studentId && autoIdentified == payment.autoIdentified && Objects.equals(dateTime, payment.dateTime) && Objects.equals(info, payment.info);
+        return id == payment.id && Double.compare(payment.sum, sum) == 0 && studentId == payment.studentId && Objects.equals(dateTime, payment.dateTime) && Objects.equals(autoIdentified, payment.autoIdentified) && Objects.equals(info, payment.info);
     }
 
     @Override
@@ -70,11 +73,76 @@ public class Payment {
     @Override
     public String toString() {
         return "Payment{" +
-                "dateTime=" + dateTime +
+                "id=" + id +
+                ", dateTime=" + dateTime +
                 ", sum=" + sum +
                 ", studentId=" + studentId +
                 ", autoIdentified=" + autoIdentified +
                 ", info='" + info + '\'' +
                 '}';
+    }
+
+    public Builder copy() {
+        return new Builder()
+                .setId(this.id)
+                .setDateTime(this.dateTime)
+                .setSum(this.sum)
+                .setStudentId(this.studentId)
+                .setAutoIdentified(this.autoIdentified)
+                .setInfo(this.info);
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private long id;
+        private LocalDateTime dateTime;
+        private double sum;
+        private long studentId;
+        private Boolean autoIdentified;
+        private String info;
+
+        public Builder setId(long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setDateTime(LocalDateTime dateTime) {
+            this.dateTime = dateTime;
+            return this;
+        }
+
+        public Builder setSum(double sum) {
+            this.sum = sum;
+            return this;
+        }
+
+        public Builder setStudentId(long studentId) {
+            this.studentId = studentId;
+            return this;
+        }
+
+        public Builder setAutoIdentified(Boolean autoIdentified) {
+            this.autoIdentified = autoIdentified;
+            return this;
+        }
+
+        public Builder setInfo(String info) {
+            this.info = info;
+            return this;
+        }
+
+        public Payment build() {
+            return new Payment(
+                    this.id,
+                    this.dateTime,
+                    this.sum,
+                    this.studentId,
+                    this.autoIdentified,
+                    this.info
+            );
+        }
     }
 }
