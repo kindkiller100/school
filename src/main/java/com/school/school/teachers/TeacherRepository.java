fa3363beff50fe58a9bc06d441617a3431bc4831
SviatlanaYatsuk <bkbb@bk.ru>
2023-01-05 18:53:56 +0300

    changed TeacherClass,TeacherRepository,TeacherService,TeachetController

---
+++ src/main/java/com/school/school/teachers/TeacherRepository.java
@@ -0,0 +1,12 @@
+package com.school.school.teachers;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher,Long>
+{
+    List<Teacher> findAll();
+}
