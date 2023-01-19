3e20e8940d089e2e95031e8ecac780e796007899
RomaSapon <rsapon@mail.ru>
2023-01-19 21:04:34 +0300

    add changes

--- src/main/resources/scripts/3_schema_in_foreign_keys.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE school_db.lessons DROP CONSTRAINT lessons_subject_id_fkey;
-ALTER TABLE school_db.lessons ADD CONSTRAINT lessons_subject_id_fkey FOREIGN KEY (subject_id) REFERENCES subjects (id) NOT NULL;
