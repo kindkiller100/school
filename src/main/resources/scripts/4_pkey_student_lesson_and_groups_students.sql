da599be19eb3c83958badc2bc89fd34b2d4edc71
yauhen14 <buapyp@gmail.com>
2023-01-24 16:05:37 +0300

    51 remove id from db table student_lesson

--- src/main/resources/scripts/4_pkey_student_lesson_and_groups_students.sql
+++ src/main/resources/scripts/4_pkey_student_lesson_and_groups_students.sql
@@ -3,10 +3,10 @@ ALTER TABLE school_db.student_lesson DROP COLUMN id;
 
 -- добавляем primary key по обеим колонкам таблицы student_lesson
 ALTER TABLE school_db.student_lesson
-    ADD CONSTRAINT student_lesson_pk
+    ADD CONSTRAINT student_lesson_pkey
         PRIMARY KEY (student_id, lesson_id);
 
 -- добавляем primary key по обеим колонкам таблицы groups_students
 ALTER TABLE school_db.groups_students
-    ADD CONSTRAINT groups_students_pk
+    ADD CONSTRAINT groups_students_pkey
         PRIMARY KEY (group_id, student_id);
