d642395934f17ba0be4c198455097094697a2b97
galkha <galkha25@gmail.com>
2023-02-09 22:10:43 +0300

    91 changes after review

--- src/main/java/com/school/school/subjects/SubjectController.java
+++ src/main/java/com/school/school/subjects/SubjectController.java
@@ -3,7 +3,14 @@ package com.school.school.subjects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
@@ -12,41 +19,41 @@ import javax.validation.Valid;
 public class SubjectController {
 
     @Autowired
-    SubjectService subjectService;
+    SubjectService service;
 
     @GetMapping
     public Page<Subject> getAll(Pageable pageable) {
-        return subjectService.getAll(pageable);
+        return service.getAll(pageable);
     }
 
     @GetMapping("/deleted")
     public Page<Subject> getAllDeleted(Pageable pageable) {
-        return subjectService.getAllDeleted(pageable);
+        return service.getAllDeleted(pageable);
     }
 
     @PostMapping
     public void create(@Valid @RequestBody Subject subject) {
-        subjectService.create(subject);
+        service.create(subject);
     }
 
     @PutMapping
     public void edit(@Valid @RequestBody Subject subject) {
-        subjectService.edit(subject);
+        service.edit(subject);
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable long id) {
-        subjectService.delete(id);
+        service.delete(id);
     }
 
     @DeleteMapping("/wipe")
     public void wipe() {
-        subjectService.wipe();
+        service.wipe();
     }
 
     @PutMapping("/{id}/restore")
     public void restoreDeleted(@PathVariable long id) {
-        subjectService.restoreDeleted(id);
+        service.restoreDeleted(id);
     }
 
 }
